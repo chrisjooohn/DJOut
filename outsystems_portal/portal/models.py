06820d456b01d055b8fe46b0cb482aa6dbb22b7e
Chris John Avila <thechrisjohnavila@gmail.com>
2023-10-17 11:21:23 +0900

    Models
    Adding models and others

--- outsystems_portal/portal/models.py
+++ outsystems_portal/portal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PORTAL_TBL(models.Model):
+    ID = models.BigAutoField(primary_key=True)
+    TITLE = models.CharField(max_length=100)
+    DESCRIPTION = models.CharField(max_length=1000)
+    INSERT_USER_ID = models.CharField(max_length=10)
+    INSERT_DATE = models.DateTimeField()
+    UPDATE_USER_ID = models.CharField(max_length=10)
+    UPDATE_DATE = models.DateTimeField()
+         
+    def __str__(self):
+        return self.ID
